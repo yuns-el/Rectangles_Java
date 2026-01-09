fdf4df47193978a6241750fec39455b0912ce14f
yunss-el <elhailoufiyounes342@gmail.com>
2026-01-09 10:51:01 +0100

    first commit

---
+++ src/main/java/org/yourcompany/yourproject/Rectangles.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package org.yourcompany.yourproject;
+
+/**
+ *
+ * @author Younes
+ */
+public class Rectangles {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
