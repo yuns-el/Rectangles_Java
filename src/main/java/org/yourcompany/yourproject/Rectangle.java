b5cac496ecbd799ed5ae28ec5210a26fe19f7c4a
yunss-el <elhailoufiyounes342@gmail.com>
2026-01-16 10:24:24 +0100

    Modificació_2

--- src/main/java/org/yourcompany/yourproject/Rectangle.java
+++ src/main/java/org/yourcompany/yourproject/Rectangle.java
@@ -2,6 +2,42 @@ package org.yourcompany.yourproject;
 
 public class Rectangle {
 
-    double amplada;
-    double llargada;
+    private double amplada;
+    private double llargada;
+
+    public Rectangle(double amplada, double llargada) {
+        setAmplada(amplada);
+        setLlargada(llargada);
+    }
+
+    public double area() {
+        return amplada*llargada;
+    }
+
+    public double perimetre() {
+        return amplada*2 + llargada*2;
+    }
+
+    public double getAmplada() {
+        return amplada;
+    }
+
+    public double getLlargada() {
+        return llargada;
+    }
+
+    public void setAmplada(double amplada) {
+        if (amplada <=0) {
+            this.amplada = 1;
+        } else
+        this.amplada = amplada;
+    }
+
+    public void setLlargada(double llargada) {
+        if (llargada <=0) {
+            this.llargada = 1;
+        } else
+        this.llargada = llargada;
+    }
+    
 }
--- src/main/java/org/yourcompany/yourproject/Rectangles.java
+++ src/main/java/org/yourcompany/yourproject/Rectangles.java
@@ -8,16 +8,27 @@ package org.yourcompany.yourproject;
  *
  * @author Younes
  */
+import java.util.Random;
+import java.util.Scanner;
 public class Rectangles {
 
     public static void main(String[] args) {
+        Scanner lector = new Scanner(System.in);
+        Random rand = new Random();
 
-        Rectangle r1 = new Rectangle();
-        r1.amplada = 100;
-        r1.llargada = 200;
+        Rectangle r1 = new Rectangle(100, 200);
 
-        Rectangle r2 = new Rectangle();
-        r2.amplada = 30;
-        r2.llargada = 60;
+        Rectangle r2 = new Rectangle(30, 60);
+
+        Rectangle r3 = new Rectangle(-30, -60);
+
+        System.out.println("L'area del r1 es: " + r1.area());
+        System.out.println("L'area del r2 es: " + r2.area());
+
+        System.out.println("El perimetre del r1 es: " + r1.perimetre());
+        System.out.println("El perimetre del r2 es: " + r2.perimetre());
+
+        System.out.println("L'amplada del rectangle 3 (amb valor negatiu): " + r3.getAmplada());
+        System.out.println("L'altura del rectangle 3 (amb valor negatiu): " + r3.getLlargada());
     }
 }
