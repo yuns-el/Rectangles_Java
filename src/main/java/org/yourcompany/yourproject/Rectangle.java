2f0f8f0e014bb06079bc8d7e10c6f7837164eafc
yunss-el <elhailoufiyounes342@gmail.com>
2026-01-21 10:02:17 +0100

    Modificació_3

--- src/main/java/org/yourcompany/yourproject/Rectangle.java
+++ src/main/java/org/yourcompany/yourproject/Rectangle.java
@@ -4,10 +4,23 @@ public class Rectangle {
 
     private double amplada;
     private double llargada;
+    private String color;
+
+    public Rectangle() {
+        setAmplada(1);
+        setLlargada(1);
+        setColor("Vermell");
+    }
 
     public Rectangle(double amplada, double llargada) {
         setAmplada(amplada);
         setLlargada(llargada);
+    }
+
+    public Rectangle (double amplada, double llargada, String color) {
+        setAmplada(amplada);
+        setLlargada(llargada);
+        this.setColor(color);
     }
 
     public double area() {
@@ -38,6 +51,29 @@ public class Rectangle {
             this.llargada = 1;
         } else
         this.llargada = llargada;
+    }
+
+    public double diagonal() {
+        return Math.sqrt((amplada*amplada) + (llargada*llargada));
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Rectangle{");
+        sb.append("amplada=").append(amplada);
+        sb.append(", llargada=").append(llargada);
+        sb.append(", color=").append(color);
+        sb.append('}');
+        return sb.toString();
     }
     
 }
--- src/main/java/org/yourcompany/yourproject/Rectangles.java
+++ src/main/java/org/yourcompany/yourproject/Rectangles.java
@@ -22,13 +22,33 @@ public class Rectangles {
 
         Rectangle r3 = new Rectangle(-30, -60);
 
+        Rectangle r4 = new Rectangle(20, 40, "blau");
+
+        Rectangle r5 = new Rectangle();
+
         System.out.println("L'area del r1 es: " + r1.area());
         System.out.println("L'area del r2 es: " + r2.area());
+        System.out.println("L'area del r3 es: " + r3.area());
 
         System.out.println("El perimetre del r1 es: " + r1.perimetre());
         System.out.println("El perimetre del r2 es: " + r2.perimetre());
+        System.out.println("El perimetre del r3 es: " + r3.perimetre());
+        
+        System.out.println("L'amplada del rectangle 1: " + r1.getAmplada());
+        System.out.println("L'amplada del rectangle 2: " + r2.getAmplada());
+        System.out.println("L'amplada del rectangle 3: " + r3.getAmplada());
 
-        System.out.println("L'amplada del rectangle 3 (amb valor negatiu): " + r3.getAmplada());
-        System.out.println("L'altura del rectangle 3 (amb valor negatiu): " + r3.getLlargada());
+        System.out.println("L'altura del rectangle 1: " + r1.getLlargada());
+        System.out.println("L'altura del rectangle 2: " + r2.getLlargada());
+        System.out.println("L'altura del rectangle 3: " + r3.getLlargada());
+
+        System.out.println("La diagonal del rectangle 1 es: " + r1.diagonal());
+        System.out.println("La diagonal del rectangle 2 es: " + r2.diagonal());
+        System.out.println("La diagonal del rectangle 3 es: " + r3.diagonal());
+
+        System.out.println("El color del rectangle 1 es: " + r1.getColor());
+        System.out.println("El color del rectangle 4 es: " + r4.getColor());
+
+        System.out.println(r5.toString());
     }
 }
